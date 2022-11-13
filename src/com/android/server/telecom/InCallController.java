de2d0a0d73304cf0d05be8e6f673e59f209e9472
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-13 22:58:02 +0530

    InCallController: Use USAGE_ACCESSIBILITY attribute for the vibrator
    - Without this, in-call vibration gets disabled on disabling touch feedback in vibration settings.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -51,6 +51,7 @@ import android.os.RemoteException;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -99,6 +100,9 @@ public class InCallController extends CallsManagerListenerBase implements
             VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
     private static final VibrationEffect CALL_DISCONNECT_EFFECT =
             VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
+    private static final VibrationAttributes IN_CALL_VIBRATION_ATTRIBUTES =
+        VibrationAttributes.createForUsage(VibrationAttributes.USAGE_ACCESSIBILITY);
+
 
     public class InCallServiceConnection {
         /**
@@ -1331,7 +1335,7 @@ public class InCallController extends CallsManagerListenerBase implements
 
     public void vibrate(VibrationEffect effect) {
         if (mVibrator.hasVibrator()) {
-            mVibrator.vibrate(effect);
+            mVibrator.vibrate(effect, IN_CALL_VIBRATION_ATTRIBUTES);
         }
     }
 
